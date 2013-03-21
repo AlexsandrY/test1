798f531aaf2db90c715b9b50e68bed406bc93377
Älexsandrgit config --global user.email Alexsandr Vladimirovichgit config --global user.name Älexsandr <Alexsandrvladimirovich@gmail.com>
2013-03-21 10:32:29 +0300

    Initial commit

---
+++ src/main/scala/main.scala
@@ -0,0 +1,14 @@
+package pro.savant.test
+
+import pro.savant.circumflex._, core._, web._, freemarker._
+import java.util.Date
+
+class Main extends Router {
+  val log = new Logger("pro.savant.test")
+
+  'currentDate := new Date
+
+  get("/test") = "I'm fine, thanks!"
+  get("/") = ftl("index.ftl")
+
+}
